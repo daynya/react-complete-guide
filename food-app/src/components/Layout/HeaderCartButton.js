ae7620d2e5d77bb55bd2c922cd990f97eaa65d46
Daynya <daynya.quigley@insight.com>
2021-07-13 09:48:43 -0400

    Adding in cart functionality

--- food-app/src/components/Layout/HeaderCartButton.js
+++ food-app/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import { useContext } from 'react';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 
 const HeaderCartButton = props => {
-    return <button className={classes.button}>
+    const cartCtx = useContext(CartContext);
+    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+        return currentNumber + item.amount;
+    }, 0);
+    return <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your Cart </span>
-        <span className={classes.badge}>3</span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
 };
 
