a2c00102524af68c6175ffb56a8ff796e06e7b27
Daynya <daynya.quigley@insight.com>
2021-09-21 13:21:05 -0400

    Starting in on custom use-http hook.

---
+++ section-15b/src/hooks/use-http.js
@@ -0,0 +1,39 @@
+import reactDom from "react-dom";
+import React, { useState } from 'react';
+
+const useHttp = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [tasks, setTasks] = useState([]);
+  
+    const sendRequest = async (taskText) => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          'https://react-http-69472.firebaseio.com/tasks.json'
+        );
+  
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+  
+        const data = await response.json();
+  
+        const loadedTasks = [];
+  
+        for (const taskKey in data) {
+          loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+        }
+  
+        setTasks(loadedTasks);
+      } catch (err) {
+        setError(err.message || 'Something went wrong!');
+      }
+      setIsLoading(false);
+    };
+
+};
+
+export default useHttp;
